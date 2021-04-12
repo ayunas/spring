af2cc4b8d62b4377d84eb7dd2f25cf6ab322d06f
Amir Yunas <ayunas@gmail.com>
2021-04-11 22:34:33 -0500

    /admin and /user endpoints for WelcomeController

--- javabrains/sec/src/main/java/io/javabrains/sec/WelcomeController.java
+++ javabrains/sec/src/main/java/io/javabrains/sec/WelcomeController.java
@@ -12,14 +12,14 @@ public class WelcomeController {
 		return "<h1>Welcome!</h1>";
 	}
 
-	@GetMapping
-	public String welcome() {
-		return "<h1>Welcome! User</h1>";
-	}
-
 	@GetMapping("/user")
 	public String welcomeUser() {
 		return "<h1>Welcome! User</h1>";
+	}
+
+	@GetMapping("/admin")
+	public String welcomeAdmin() {
+		return "<h1>Welcome! Admin</h1>";
 	}
 
 
